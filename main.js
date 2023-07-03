a05fe96e4f53ad069055e3f9873ae410de39586d
john makateto <jmakateto@gmail.com>
2023-07-03 11:28:13 +0300

    completed project

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
-// Add event listener to the purchase button
-const purchaseBtn = document.getElementById('purchaseBtn');
-if (purchaseBtn) {
-  purchaseBtn.addEventListener('click', handlePurchase);
-}
-
-// Add event listeners to the delete buttons
-const deleteButtons = document.querySelectorAll('.deleteBtn');
-deleteButtons.forEach(btn => {
-  btn.addEventListener('click', handleDelete);
-});
-
-// Function to handle the purchase button click
-function handlePurchase() {
-  // Logic for purchasing a ticket
-  // Send the appropriate request to the server
-}
-
-// Function to handle the delete button click
-function handleDelete(event) {
-  // Logic for deleting a film
-  // Send the appropriate request to the server
-}
+// Function to render films
+function renderFilms(films) {
+    const filmsList = document.getElementById('films');
+    filmsList.innerHTML = '';
+  
+    films.forEach(film => {
+      const filmItem = document.createElement('li');
+      filmItem.innerHTML = `
+        <span>${film.title}</span>
+        <button class="deleteBtn" data-id="${film.id}">Delete</button>
+      `;
+      filmsList.appendChild(filmItem);
+    });
+  
+    // Add event listeners to the delete buttons
+    const deleteButtons = document.querySelectorAll('.deleteBtn');
+    deleteButtons.forEach(btn => {
+      btn.addEventListener('click', handleDelete);
+    });
+  }
+  
+  // Function to handle the delete button click
+  function handleDelete(event) {
+    const filmId = event.target.dataset.id;
+    
+    // Make a DELETE request to the server
+    fetch(`/films/${filmId}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        // Remove the film from the DOM
+        const filmItem = event.target.parentElement;
+        filmItem.remove();
+      })
+      .catch(error => {
+        console.error('Error deleting film:', error);
+      });
+  }
+  
+  // Sample films data
+  const films = [
+    { id: '1', title: 'Film 1' },
+    { id: '2', title: 'Film 2' },
+    { id: '3', title: 'Film 3' }
+  ];
+  
+  // Render films on page load
+  document.addEventListener('DOMContentLoaded', () => {
+    renderFilms(films);
+  });
+  
